42167054210e9b6995cefee179291c3b7509e76c
aman42jha <amankumarjha42@gmail.com>
2019-04-14 17:44:00 +0530

    Updates

---
+++ precache-manifest.8cb8ac29a5cea62349eb0abc1ab1e3ae.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/news-headlines/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "402578dbe59a04544e75",
+    "url": "/news-headlines/static/css/main.bff76960.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/news-headlines/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3c2c3bf96a4f2dcb498f",
+    "url": "/news-headlines/static/js/1.3c2c3bf9.chunk.js"
+  },
+  {
+    "revision": "356c5c627f642c7ad245",
+    "url": "/news-headlines/static/js/runtime~main.356c5c62.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/news-headlines/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/news-headlines/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/news-headlines/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/news-headlines/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/news-headlines/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/news-headlines/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/news-headlines/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/news-headlines/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "402578dbe59a04544e75",
+    "url": "/news-headlines/static/js/main.402578db.chunk.js"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/news-headlines/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/news-headlines/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/news-headlines/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/news-headlines/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/news-headlines/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/news-headlines/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/news-headlines/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/news-headlines/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/news-headlines/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/news-headlines/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/news-headlines/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3c2c3bf96a4f2dcb498f",
+    "url": "/news-headlines/static/css/1.6612bd80.chunk.css"
+  },
+  {
+    "revision": "eb49b89abce968ea329fc99752336c1a",
+    "url": "/news-headlines/index.html"
+  }
+];
